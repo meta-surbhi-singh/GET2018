4e4a9b3f4d85da24e45074b5c8568d40c2fd5cae
meta-surbhi-singh <41181739+meta-surbhi-singh@users.noreply.github.com>
2018-07-19 01:36:02 -0700

    Delete Promotion.java

--- src/Promotion.java
+++
@@ -1,9 +0,0 @@
-public interface Promotion
-{
-public float getMinimumPrice();
-public void setMinimumPrice(int minimumPrice);
-public float getFixedDiscount();
-public void setFixedDiscount(int fixedDiscount);
-public boolean isPromotionApplicable(String code);
-
-}
