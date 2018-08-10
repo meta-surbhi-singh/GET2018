7e50d9c274ac04957561d3e106ad5142ae9ea7d5
meta-surbhi-singh <41181739+meta-surbhi-singh@users.noreply.github.com>
2018-08-10 05:13:15 -0700

    Delete NQueenRunner.java

--- nQueen/NQueenRunner.java
+++
@@ -1,8 +0,0 @@
-package nQueen;
-public class NQueenRunner {
-	  public static void main(String args[])
-	    {
-	        NQueenProblem Queen = new NQueenProblem(4);
-	        Queen.solveNQ();
-	    }
-}
